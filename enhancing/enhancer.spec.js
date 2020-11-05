6c79694de628a9880772191774f52af340f81f46
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-11-04 22:34:37 -0500

    added repair test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("enhancer tests", () =>{
+    describe("repair", () => {
+        it("should take in an item and restore it's durabilty", () =>{
+            expect(enhancer.repair({
+                name: "magic wond",
+                durability: 10, 
+                enhancement: 10,
+            })).toEqual({
+                name: "magic wond",
+                durability: 100,
+                enhancement : 10,
+            })
+        })
+    })
+})
